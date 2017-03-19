8100186a76726c44ff5feb8759adb71495640808
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-20 05:29:59 +0700

    buat abstract class social thing

---
+++ SocialThing.php
@@ -0,0 +1,14 @@
+<?php
+abstract class SocialThing {
+  private $content;
+
+  public function __construct($content) {
+    $this->content = $content;
+  }
+
+  public function content() {
+    return $this->content;
+  }
+}
+
+?>
